8b2c75a86d6cb7d2995233c24b8d99683f115a34
elmyr <elmyr.vtuber@gmail.com>
2018-11-23 14:38:50 +0900

    add Blog

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('post_main/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post_main/', views.post_main, name='post_main'),
 ]
