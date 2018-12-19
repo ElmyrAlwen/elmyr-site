4ac138f727f14778b8becfb972ea97403d4b02f6
elmyr <elmyr.vtuber@gmail.com>
2018-12-19 15:28:11 +0900

    add gallery

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post_main/<int:pk>/', views.post_detail, name='post_detail'),
     path('post_main/', views.post_main, name='post_main'),
+    path('gallery/', views.gallery, name='gallery'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,7 @@ def post_main(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def gallery(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/gallery.html', {'posts': posts})
