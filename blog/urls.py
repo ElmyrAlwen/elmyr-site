903715bea1135deb264cf75398aa6ae0950c28b1
elmyr <elmyr.vtuber@gmail.com>
2018-12-24 11:28:05 +0900

    add links

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('post_main/<int:pk>/', views.post_detail, name='post_detail'),
     path('post_main/', views.post_main, name='post_main'),
     path('gallery/', views.gallery, name='gallery'),
+    path(r'^post/(?P<pk>\d+)/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
